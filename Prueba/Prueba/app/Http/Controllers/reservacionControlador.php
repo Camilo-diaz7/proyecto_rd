6a14ad1dc18e807df19804b6e85a037d38d8d77c
Camilo-diaz7 <camilo.escuderos10@gmail.com>
2025-09-24 19:13:44 -0500

    add
    reservacion 2.0

--- Prueba/Prueba/app/Http/Controllers/reservacionControlador.php
+++ Prueba/Prueba/app/Http/Controllers/reservacionControlador.php
@@ -31,7 +31,7 @@ class reservacionControlador extends Controller
     }
     if($usuario->role=='admin'){
         $reservaciones= Reservacion::all();
-        return view('admin.reservaciones.index',compact('reservaciones'));
+        return view('empleados.reservaciones.index',compact('reservaciones'));
     }
 
     abort(403, 'Acceso denegado');
