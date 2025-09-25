9fb298dccd065d5d63a60940421d4f4b3f2f59af
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2025-09-24 20:50:54 -0500

    add: añadir producto

--- Prueba/Prueba/resources/views/empleados/productos/index.blade.php
+++ Prueba/Prueba/resources/views/empleados/productos/index.blade.php
@@ -17,6 +17,7 @@
                 <th>Tipo</th>
                 <th>Stock</th>
                 <th>Precio</th>
+                <th>Imagen</th>
                 <th>Acciones</th>
             </tr>
         </thead>
@@ -27,6 +28,16 @@
                 <td>{{ $producto->nombre }}</td>
                 <td>{{ $producto->tipo_producto }}</td>
                 <td>{{ $producto->stock }}</td>
+                <td>
+            @if($producto->imagen)
+             <img src="{{ asset('storage/' . $producto->imagen) }}" 
+             alt="Imagen de {{ $producto->nombre }}" 
+             width="80" height="80"
+             style="object-fit: cover; border-radius: 5px;">
+             @else
+             <span class="text-muted">Sin imagen</span>
+              @endif
+            </td>
                 <td>${{ number_format($producto->precio_unitario, 3) }}</td>
                 <td>
                     <a href="{{ route('admin.productos.edit', $producto) }}" class="btn btn-warning btn-sm">Editar</a>
